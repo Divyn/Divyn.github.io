58bd78ae1f671619f0809b2dc6a0fe79fedf4e92
divyn <divyn0108@gmail.com>
2025-03-24 12:31:09 +0530

    new build with videos

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,52 +3,17 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/markdown-page',
     component: ComponentCreator('/markdown-page', '3d7'),
     exact: true
   },
   {
     path: '/docs',
-    component: ComponentCreator('/docs', '595'),
+    component: ComponentCreator('/docs', '630'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', 'ad9'),
+        component: ComponentCreator('/docs', '210'),
         routes: [
           {
             path: '/docs/tags',
@@ -72,7 +37,7 @@ export default [
           },
           {
             path: '/docs',
-            component: ComponentCreator('/docs', '804'),
+            component: ComponentCreator('/docs', '42d'),
             routes: [
               {
                 path: '/docs/category/crm',
@@ -166,7 +131,7 @@ export default [
               },
               {
                 path: '/docs/featured-work/video-tutorials',
-                component: ComponentCreator('/docs/featured-work/video-tutorials', '058'),
+                component: ComponentCreator('/docs/featured-work/video-tutorials', 'aa2'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
